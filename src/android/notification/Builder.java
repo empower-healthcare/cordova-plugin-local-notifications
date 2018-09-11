45c37912394be627ad1a05d7f91686d284750c4a
ToshiakiOshizawa <toshiaki.oshizawa@sbr-inc.co.jp>
2018-09-11 16:52:53 +0900

    修正

--- src/android/notification/Builder.java
+++ src/android/notification/Builder.java
@@ -28,6 +28,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
